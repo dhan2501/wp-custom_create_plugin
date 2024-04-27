6556f172576002c185b2a8d8d0d98e84034e3b1a
dhan2501 <guptadhananjay10@gmail.com>
2024-04-27 05:45:16 +0530

    I have create activation, deactivation, uninstall plugin code

--- plugins/my-plugin/my-plugin.php
+++ plugins/my-plugin/my-plugin.php
@@ -21,11 +21,30 @@ if (!defined('ABSPATH')){
 
 
 function my_plugin_activation(){
+    global $wpdb, $table_prefix;
+    $wp_emp = $table_prefix.'emp';
+    $q = "CREATE TABLE IF NOT EXISTS `$wp_emp` (`id` INT(10) NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL , `status` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    $wpdb->query($q);
 
+    // $q = "INSERT INTO `$wp_emp` (`name`, `email`, `status`) VALUES ('Dhananjay', 'dhananjay@gmail.com', 1)";
+    // $wpdb->query($q);
+    $data = array(
+        'name' => 'Akshay',
+        'email' => 'aksha@gmail.com',
+        'status' => 1
+    );
+
+    $wpdb->insert($wp_emp, $data);
 }
 register_activation_hook(__FILE__, 'my_plugin_activation');
-function my_plugin_deactivation(){
 
+function my_plugin_deactivation(){
+    global $wpdb, $table_prefix;
+    $wp_emp = $table_prefix.'emp';
+
+    // $q = "DROP TABLE `$wp_emp`";
+    $q = "TRUNCATE TABLE `$wp_emp`";
+    $wpdb->query($q);
 
 }
 register_deactivation_hook(__FILE__, 'my_plugin_deactivation');
