6c65880c9cd16f2a10b4034e436fa4c771c43616
dhan2501 <guptadhananjay10@gmail.com>
2024-04-28 20:39:34 +0530

    get data from database and fetch data

---
+++ plugins/my-plugin/img_gallery.php
@@ -0,0 +1,8 @@
+<div>
+    <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" alt="">
+    <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" alt="">
+    <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" alt="">
+    <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" alt="">
+    <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" alt="">
+    <img src="https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg" alt="">
+</div>
--- plugins/my-plugin/my-plugin.php
+++ plugins/my-plugin/my-plugin.php
@@ -23,7 +23,7 @@ if (!defined('ABSPATH')){
 function my_plugin_activation(){
     global $wpdb, $table_prefix;
     $wp_emp = $table_prefix.'emp';
-    $q = "CREATE TABLE IF NOT EXISTS `$wp_emp` (`id` INT(10) NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL , `status` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
+    $q = "CREATE TABLE IF NOT EXISTS `$wp_emp` (`id` INT(10) NOT NULL AUTO_INCREMENT , `name` VARCHAR(50) NOT NULL , `email` VARCHAR(100) NOT NULL , `phone` VARCHAR(50) NOT NULL , `status` BOOLEAN NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;";
     $wpdb->query($q);
 
     // $q = "INSERT INTO `$wp_emp` (`name`, `email`, `status`) VALUES ('Dhananjay', 'dhananjay@gmail.com', 1)";
@@ -53,12 +53,14 @@ register_deactivation_hook(__FILE__, 'my_plugin_deactivation');
 // Create shortcode using php
 
 
-function my_shortcode($atts){
-    $atts = array_change_key_case($atts, CASE_LOWER);
-    $atts = shortcode_atts(array(
-        'msg' => 'I am Good',
-        'note' => 'default'
-    ), $atts);
+// function my_shortcode($atts){
+//     // $atts = array_change_key_case($atts, CASE_LOWER);
+//     $atts = array_change_key_case((array) $atts, CASE_LOWER);
+//     $atts = shortcode_atts(array(
+//         'type' => 'img_gallery',
+//         // 'note' => 'default'
+
+//     ), $atts);
 
     // ob_start();
     ?>
@@ -68,7 +70,103 @@ function my_shortcode($atts){
 
     // return 'Result: '. $atts['msg'];
     // return $html;
-    include 'notice.php';
+    // include 'slider.php';
+    // include $atts['type'].'.php';
+// }
+// add_shortcode('my-sc','my_shortcode');
+
+function my_custom_scripts(){
+$path_js = plugins_url('js/main.js', __FILE__);
+$path_style = plugins_url('css/style.css', __FILE__);
+$dep = array('jquery');
+$ver = filemtime(plugin_dir_path(__FILE__).'js/main.js');
+$ver_style = filemtime(plugin_dir_path(__FILE__).'css/style.css');
+$is_login = is_user_logged_in() ? 1 : 0;
+
+//style enqueue
+wp_enqueue_style('my-custom-style', $path_style, '', $ver_style);
+
+//script includes
+wp_enqueue_script('my-custom-js', $path_js, $dep, $ver, true); //dependencies
+wp_add_inline_script('my-custom-js', 'var is_login = '.$is_login.';','before');
+
+// if(is_page('home')){
+//     wp_enqueue_script('my-custom-js', $path_js, $dep, $ver, true); //dependencies
+// }
 }
-add_shortcode('my-sc','my_shortcode');
+add_action('wp_enqueue_scripts','my_custom_scripts');
+add_action('admin_enqueue_scripts','my_custom_scripts');
+
+
+//create shortcode
+function my_custom_shortcode()
+{
+    global $wpdb, $table_prefix;
+    $wp_emp = $table_prefix.'emp';
+    $q = "SELECT * FROM `$wp_emp`";
+    $results = $wpdb->get_results($q);
+
+    // print_r($result);
+    ob_start();
+?>
+<table>
+    <thead>
+    <tr>
+        <td>Id</td>
+        <td>Name</td>
+        <td>Email</td>
+        <td>Phone</td>
+    </tr>
+</thead>
+<tbody>
+    <?php
+    foreach($results as $row){
+    ?>
+    <tr>
+        <td><?php echo $row->id; ?></td>
+        <td><?php echo $row->name; ?></td>
+        <td><?php echo $row->email; ?></td>
+        <td><?php echo $row->phone; ?></td>
+    </tr>
+    <?php
+    }
+?>
+</tbody>
+</table>
+<?php
+    $html = ob_get_clean();
+    return $html;
+
+}
+add_shortcode('my-sc', 'my_custom_shortcode');
+
+
+// Get Post data using shortcode
+
+function my_posts(){
+    $args = array(
+        'post_type' => 'post'
+    );
+    $query = new WP_Query($args);
+    ob_start();
+    if($query->have_posts()):
+    ?>
+    <ul>
+        <?php
+            while($query->have_posts()){
+                $query->the_post();
+                echo '<li>'.get_the_title().'->'.get_the_content().'</li>';
+            }
+        ?>
+        
+    </ul>
+    <?php
+           
+    endif;
+    wp_reset_postdata();
+    $html = ob_get_clean();
+    return $html;
+
+}
+add_shortcode('my-post','my_posts');
 
